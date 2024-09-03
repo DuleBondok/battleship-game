c806fe70332e9e4ab7ed067984a5d5ead54ae5a5
Dušan Bondokić <dulebondok@gmail.com>
2024-09-03 17:34:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,4 @@ let playAgain = document.getElementById("playAgain");
 playAgain.addEventListener("click", () => {
   location.reload();
 });
-// adding e.listener to gitHub Img
-let gitHub = document.getElementById("gitHub");
-gitHub.addEventListener("click", () => {
-  window.open("https://github.com/DuleBondok");
-});
 
